b1058fddd874364a642cdc80b3308a1f3ec403e5
jorgebeauregard <jorgebeauregard@outlook.com>
2018-11-06 19:58:32 -0600

    Updates

---
+++ precache-manifest.ffe6c5363605aa5f8be291857274c3e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f0c06bbd61448131862f",
+    "url": "/foodplus/build/static/js/runtime~main.f0c06bbd.js"
+  },
+  {
+    "revision": "ec4597b70f58500929f5",
+    "url": "/foodplus/build/static/js/main.ec4597b7.chunk.js"
+  },
+  {
+    "revision": "be07c53e533da58dab2d",
+    "url": "/foodplus/build/static/js/1.be07c53e.chunk.js"
+  },
+  {
+    "revision": "ec4597b70f58500929f5",
+    "url": "/foodplus/build/static/css/main.38db4926.chunk.css"
+  },
+  {
+    "revision": "16a7c98b02724af2944b546aa7ef674f",
+    "url": "/foodplus/build/index.html"
+  }
+];
